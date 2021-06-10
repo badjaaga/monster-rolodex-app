01c18f43209a52d49db3420833fd3fdfc2321e40
nadzeya molchan <nadzeya.molchan@itechart-group.com>
2021-06-10 19:02:00 +0300

    add styling, add search

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import React from "react";
 import axios from 'axios';
 
-import {SpacingGrid} from './components/card-field/card-field.component';
+import {CardField} from "./components/card-field/card-field.component";
+import {SearchBox} from "./components/search-box/search-box.component";
+
+import './App.css';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: '',
         };
     }
+
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => this.setState({monsters: response.data}))
     }
 
+    handleChange = (e) => {
+        this.setState({searchField: e.target.value});
+    }
+
     render() {
+        const { monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
         return (
-            <SpacingGrid monsters={this.state.monsters}/>
+            <div className='container'>
+                <h1>Monster rolodex</h1>
+                <SearchBox placeholder='search monsters'
+                           handleChange={this.handleChange}
+                />
+                <CardField monsters={filteredMonsters}/>
+            </div>
         )
     }
 }
 
 export default App;
-
-/*<div>
-    {
-        this.state.monsters.map(monster => <h1>{monster.name}</h1>)
-    }
-</div>*/
--- src/components/card-field/card-field.component.jsx
+++ src/components/card-field/card-field.component.jsx
@@ -1,46 +1,11 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import './card-field.component.style.css';
+import {Card} from "../card/card.component";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        height: 400,
-        width: 300,
-    },
-    control: {
-        padding: theme.spacing(6),
-    }
-}));
-
-export function SpacingGrid(props) {
-    console.log(props);
-    const [spacing, setSpacing] = React.useState(2);
-    const classes = useStyles();
-
-    const handleChange = (event) => {
-        setSpacing(Number(event.target.value));
-    };
-
-    return (
-        <Grid container className={classes.root} spacing={6}>
-            <Grid item xs={12}>
-                <Grid container justify="center" spacing={spacing}>
-                    {props.monsters.map((monster) => (
-                        <Grid key={monster.id} item>
-                            <Paper container justify="center" className={classes.paper}>
-                                <img src={`https://robohash.org/${monster.id}?set=set2`}/>
-                                <h3>{monster.name}</h3>
-                            </Paper>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Grid>
-            <Grid item xs={12}>
-            </Grid>
-        </Grid>
-    );
-}
+export const CardField = props => (
+    <div className='card-list'>
+        {props.monsters.map(monster => (
+            <Card key={monster.id} monster={monster}/>
+        ))}
+    </div>
+)
